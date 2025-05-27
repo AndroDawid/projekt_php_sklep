9f89826a36a90f6fbdd3e574291682d5d167cca9
AndroDawid <szelek5100@gmail.com>
2025-05-27 11:14:14 +0200

    Dodanie opcji wylogowania się

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $host = 'localhost';
 $db = 'user_db';
 $user = 'root';
@@ -9,28 +11,33 @@ $conn = new mysqli($host, $user, $pass, $db);
 $zapytanie = mysqli_query($conn, "SELECT id_produktu, nazwa, cena, ilosc, zdjecie FROM `produkty`;");
 ?>
 
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <title>Strona Główna - Primolek</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
-    <script src="js/script.js"></script>
-</head>
-<body>
-<header>
-   <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
-    <a href="index.php"><h1>Primolek</h1></a>
-    <div id="clock"></div>
-    <nav>
-        <ul>
-            <li><a href="index.php">Strona Główna</a></li>
-            <li><a href="logowanie.html">Logowanie</a></li>
-            <li><a href="koszyk.php">Koszyk</a></li>
-        </ul>
-    </nav>
-</header>
+    <!DOCTYPE html>
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Strona Główna - Primolek</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+    <header>
+        <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
+        <a href="index.php"><h1>Primolek</h1></a>
+        <div id="clock"></div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Strona Główna</a></li>
+                <?php if (!isset($_SESSION['username'])): ?>
+                    <li><a href="logowanie.html">Logowanie</a></li>
+                <?php endif; ?>
+                <li><a href="koszyk.php">Koszyk</a></li>
+                <?php if (isset($_SESSION['username'])): ?>
+                    <li><a href="logout.php">Wyloguj się</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
     <main>
         <h2>Produkty</h2>
         <?php
@@ -41,10 +48,14 @@ $zapytanie = mysqli_query($conn, "SELECT id_produktu, nazwa, cena, ilosc, zdjeci
                 echo '<h3>' . $wiersz['nazwa'] . '</h3>';
                 echo '<p>Cena: ' . $wiersz['cena'] . ' zł</p>';
                 echo '<p>Ilość: ' . $wiersz['ilosc'] . '</p>';
-                echo '<form method="POST" action="koszyk.php">';
-                echo '<input type="hidden" name="product_id" value="' . $wiersz['id_produktu'] . '">';
-                echo '<button type="submit">Dodaj do koszyka</button>';
-                echo '</form>';
+                if (isset($_SESSION['username'])) {
+                    echo '<form method="POST" action="koszyk.php">';
+                    echo '<input type="hidden" name="product_id" value="' . $wiersz['id_produktu'] . '">';
+                    echo '<button type="submit">Dodaj do koszyka</button>';
+                    echo '</form>';
+                } else {
+                    echo '<a href="logowanie.html"><button>Zaloguj się</button></a>';
+                }
                 echo '</div>';
             }
         } else {
@@ -56,8 +67,8 @@ $zapytanie = mysqli_query($conn, "SELECT id_produktu, nazwa, cena, ilosc, zdjeci
         <p>&copy; 2025 Dawid Szelągiewicz Wszelkie prawa zastrzeżone.</p>
         <p>Ul.XYZ Miasto xx-xxx +48 000 000 000</p>
     </footer>
-</body>
-</html>
+    </body>
+    </html>
 <?php
 mysqli_close($conn);
 ?>
